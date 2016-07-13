08a36c6731172432439027bc67e6420157d90c4a
anil <anilugale1989@gmail.com>
2016-07-13 10:35:43 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/example/anil/fabdemo/MainActivityTest.java
@@ -0,0 +1,47 @@
+package com.example.anil.fabdemo;
+
+
+import android.os.SystemClock;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab), isDisplayed()));
+        floatingActionButton.perform(click());
+
+
+
+
+
+        ViewInteraction floatingActionButton3 = onView(
+                allOf(withId(R.id.fab), isDisplayed()));
+
+        floatingActionButton3.perform(click());
+
+    }
+
+}
